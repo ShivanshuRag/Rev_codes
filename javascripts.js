0f370827abd0445d2ceb8b2eb425a0dd9fe83fd5
ShivanshuRag <ragsingh773@gmail.com>
2024-12-29 23:25:31 +0530

    object .assign() method

--- javascripts.js
+++ javascripts.js
@@ -37,3 +37,16 @@ console.log(obj["age"])
 
  obj.email = "hitesh@microsoft.com"
 // console.log(obj);
+
+// ======================
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+const obj4 = {5: "a", 6: "b"}
+
+// const obj3 = { obj1, obj2 }
+
+const obj3 = Object.assign({}, obj1, obj2, obj4)
+console.log(obj3); 
+
+// output- { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
