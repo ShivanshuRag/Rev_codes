7985ebe77c2e78745803f53c6ec2b8e2d9114af8
ShivanshuRag <ragsingh773@gmail.com>
2024-12-30 16:12:14 +0530

    rev

--- javascripts.js
+++ javascripts.js
@@ -14,7 +14,7 @@ const obj ={
 }
 // console.log(obj.fullname); ase bhi kar sakte hai 
 
-console.log(obj["age"])
+// console.log(obj["age"])
  // ase bhi kar skte h bcz its best practice to access key 
 
  
@@ -47,7 +47,7 @@ const obj4 = {5: "a", 6: "b"}
 // const obj3 = { obj1, obj2 }
 
 const obj3 = Object.assign({}, obj1, obj2, obj4)
-console.log(obj3); 
+// console.log(obj3); 
 
 
 // output- { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
@@ -55,3 +55,42 @@ console.log(obj3);
 const obj5 = {...obj1, ...obj2 , ...obj4} // destructure
 
 // output - { '1': 'a', '2': 'b', '3': 'a', '4': 'b', '5': 'a', '6': 'b' }
+
+
+
+const user = {
+    username: "shivanshu",
+    price: 999,
+
+    welcomeMessage: function() {
+        console.log(`${this.username} , welcome to website`);
+        console.log(this);
+    }
+
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+// console.log(this);
+
+
+//  function chai () {
+//     let username = "hitesh"
+//     console.log(this);
+   
+// }
+
+
+// chai()
+
+
+// const addTwo = (num1, num2) =>  num1 + num2
+
+// const addTwo = (num1, num2) => ( num1 + num2 )
+
+const addTwo = (num1, num2) => ( {username: "hitesh"} )
+   
+
+console.log(addTwo(3, 4))
