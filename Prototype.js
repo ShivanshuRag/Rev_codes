1de8f3eaef5e4dc70695f7029dc8f142837d7122
ShivanshuRag <ragsingh773@gmail.com>
2025-01-03 09:45:57 +0530

    call , setter getter

--- Prototype.js
+++ Prototype.js
@@ -52,3 +52,56 @@ const TeachingSupport = {
 
 
 Object.setPrototypeOf(TeachingSupport ,Teacher)
+
+
+///  ====== call ========
+
+function SetUsername(username){
+    //complex DB calls
+    this.username = username
+    // console.log("called");
+}
+
+function userdetails(username , email , passowrd){
+   SetUsername.call(this ,username)
+  this.email = email
+  this.passowrd = passowrd
+}
+
+const user1 = new userdetails(" rag ", "rag2@.com" , "21334")
+
+// console.log(user1);
+
+
+//       ======= getter - setter =========
+
+
+  class Users {
+    constructor(userName , email , passowrd  ){
+        this.userName = userName
+        this.email = email
+        this.passowrd = passowrd
+
+    }
+
+     get email(){
+        return this._email.toUpperCase()
+     }
+
+     set email(value){
+        return this._email = value
+     }
+
+     get passowrd(){
+        return `${this._passowrd}heyshiv`
+     }
+     set passowrd(value){
+        return this._passowrd = value
+     }
+ }
+
+ const user2 = new Users("rag" , " rag@.com",  234)
+ console.log(user2);
+
+ console.log(user2.email);
+ console.log(user2.passowrd);
