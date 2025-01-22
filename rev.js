6d17e2f29c5ea97d2f7d8d291f5f63ef2385193a
ShivanshuRag <ragsingh773@gmail.com>
2025-01-22 19:52:18 +0530

    rev

--- rev.js
+++ rev.js
@@ -8,13 +8,48 @@
 const one = ()=> Promise.resolve("one")
  
 
-async function two(){
-    console.log("two");
-    const result = await one() // agar await lagta hai to aync nature , go into micro task, agar await nhi lagtae hai to call stack se hi execute ho jayega
-    console.log(result);
+// async function two(){
+//     console.log("two");
+//     const result = await one() // agar await lagta hai to aync nature , go into micro task, agar await nhi lagtae hai to call stack se hi execute ho jayega
+//     console.log(result);
+//  }
+
+// console.log("start");
+// two()
+
+// console.log("end");
+
+
+function highOrder( func , arg){
+   return func(arg)
 }
+console.log(highOrder(Math.sqrt , 16)); 
 
-console.log("start");
-two()
 
-console.log("end");
+ function outerfunc (){
+    const outervar = 'this is outer variable'
+
+    function innerfunc(){
+        console.log(outervar);
+    }
+
+    return innerfunc
+ }
+
+ const closure = outerfunc()
+  closure();
+
+
+
+;(function() {
+    // Private variables and functions
+    const name = 'John';
+    function sayHello() {
+      console.log('Hello, ' + name + '!');
+    }
+  
+    // Public functions
+    function greet() {
+      console.log('Hello, world!');
+    }
+  })();
