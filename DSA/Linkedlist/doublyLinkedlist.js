bc9a371b8c509183f795887ae0c57f69fa07af4c
ShivanshuRag <ragsingh773@gmail.com>
2025-02-03 09:10:52 +0530

    Add printList and insertAtBeginning methods to doublyLinkedList class

--- DSA/Linkedlist/doublyLinkedlist.js
+++ DSA/Linkedlist/doublyLinkedlist.js
@@ -15,4 +15,32 @@ class doublyLinkedList{
         this.head = null
         this.tail = null
     }
+
+
+    printList() {
+      let current = this.head;
+      while (current) {
+        console.log(current.data);
+        current = current.next;
+      }
+    }
 }
+
+doublyLinkedList.prototype.insertAtBegining = function( node){
+  let newNode = new Node(node)
+   
+   newNode.next = this.head 
+   newNode.tail = this.prev
+   this.head = newNode
+   
+  
+}
+
+let newLinkedList = new doublyLinkedList()
+
+newLinkedList.insertAtBegining(5)
+newLinkedList.insertAtBegining(4)
+newLinkedList.insertAtBegining(3)
+newLinkedList.insertAtBegining(2)
+
+newLinkedList.printList()
