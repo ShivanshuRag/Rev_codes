db4575c4fa1ab18b4917e7d90e9ee8a9fd72c3e2
ShivanshuRag <ragsingh773@gmail.com>
2025-02-05 17:43:19 +0530

    Add initial implementation of Binary Search Tree and BSTNode class

---
+++ DSA/BinarySearchTree/BST.js
@@ -0,0 +1,33 @@
+
+
+/* 
+    Binary search Tree ka implemention 
+
+    Bst Node ke liye 
+    * key (data)
+    * left -> null
+    * right -> null
+    
+   Binary search tree
+   
+   * root -> null
+
+
+*/
+
+
+
+  class BSTNode{
+
+    constructor(key){
+        this.key = key
+        this.left = null
+        this.right = null
+    }
+  }
+
+  class BinarySearchTree{
+      constructor(){
+        this.root = null
+      }
+  }
