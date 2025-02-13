29bd75bb24bf268616ef4b53b1887bcc47f0195b
ShivanshuRag <ragsingh773@gmail.com>
2025-02-13 09:48:44 +0530

    Add iterative reverse string function and comment out example calls

--- DSA/Recursion/index.js
+++ DSA/Recursion/index.js
@@ -10,7 +10,7 @@ function reverseString(string){
      return  string[string.length - 1]  + reverseString(string.substring(0, string.length - 1))
 }
 
-console.log(reverseString("Shiavnshu"));
+// console.log(reverseString("Shiavnshu"));
 
 
 //  method 2
@@ -28,7 +28,21 @@ function reverseString(string){
         return result;
 }
 
-console.log(reverseString("Shiavnshu"));
+// console.log(reverseString("Shiavnshu"));
 
 
 // by itertive way
+
+
+function reverseStr(string) {
+    let reverse = "";
+    let index = string.length - 1; // Start from the last character
+
+    while (index >= 0) {
+        reverse += string[index]; // Append the character to the reverse string
+        index--; // Move to the previous character
+    }
+    return reverse;
+}
+
+console.log(reverseStr("shiavnshu")); 
