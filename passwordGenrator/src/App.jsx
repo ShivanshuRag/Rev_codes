680be0a040cecba9f47ef900177c495543d61911
ShivanshuRag <ragsingh773@gmail.com>
2025-05-14 23:40:29 +0530

    add more feature

--- passwordGenrator/src/App.jsx
+++ passwordGenrator/src/App.jsx
@@ -1,16 +1,20 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 
 import "./App.css";
 
 function App() {
   const [charlength, setCharLength] = useState(5);
-  // const [number, setNumber] = useState(false);
-  // const [char, setChar] = useState(false);
+  const [number, setNumber] = useState(false);
+  const [char, setChar] = useState(false);
   const [input, setInput] = useState("");
 
+  let passwordRef = useRef(null);
   function password() {
     let str = "";
     let Alpha = "ABCDEFGHIJKLMNOPQUSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    if (number) Alpha += "0123456789";
+    if (char) Alpha += "!~@#$%^&*()+=_";
 
     for (let i = 0; i <= charlength - 1; i++) {
       let random = Math.floor(Math.random() * Alpha.length);
@@ -22,10 +26,11 @@ function App() {
 
   useEffect(() => {
     password();
-  }, [charlength]);
+  }, [charlength, number, char]);
 
   function copyPassword() {
-    navigator.clipboard.writeText(input);
+    passwordRef.current?.select();
+    window.navigator.clipboard.writeText(input);
   }
   return (
     <>
@@ -36,10 +41,11 @@ function App() {
               type="text"
               value={input}
               onChange={(e) => e.target.value}
+              ref={passwordRef}
               className=" text-amber-700 p-2 rounded-lg  border-2 w-auto outline-none"
             />
             <button
-              className=" bg-blue-800 text-white font-bold p-2 hover:bg-blue-700 rounded-lg"
+              className=" bg-blue-800 text-white font-bold p-2 hover:bg-blue-700 rounded-lg "
               onClick={copyPassword}
             >
               copy
@@ -57,10 +63,18 @@ function App() {
             />
             <label htmlFor="">Length({charlength})</label>
 
-            <input type="checkbox" className="mr-1 ml-2" />
+            <input
+              type="checkbox"
+              className="mr-1 ml-2"
+              onChange={(e) => setNumber(e.target.value)}
+            />
             <label htmlFor="">Number</label>
 
-            <input type="checkbox" className="mr-1 ml-2" />
+            <input
+              type="checkbox"
+              className="mr-1 ml-2"
+              onChange={(e) => setChar(e.target.value)}
+            />
             <label htmlFor="">Character</label>
           </div>
         </div>
