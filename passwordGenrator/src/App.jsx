c3ff90fc09e68216c0ac44b005e48558a6bd72d1
ShivanshuRag <ragsingh773@gmail.com>
2025-05-14 11:38:33 +0530

    password genrate

---
+++ passwordGenrator/src/App.jsx
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+
+import "./App.css";
+
+function App() {
+  const [charlength, setCharLength] = useState(5);
+  // const [number, setNumber] = useState(false);
+  // const [char, setChar] = useState(false);
+  const [input, setInput] = useState("");
+
+  function password() {
+    let str = "";
+    let Alpha = "ABCDEFGHIJKLMNOPQUSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+
+    for (let i = 0; i <= charlength - 1; i++) {
+      let random = Math.floor(Math.random() * Alpha.length);
+      str += Alpha[random];
+    }
+
+    setInput(str);
+  }
+
+  useEffect(() => {
+    password();
+  }, [charlength]);
+
+  function copyPassword() {}
+
+  return (
+    <>
+      <div className=" justify-center ">
+        <div className=" flex-col w-1/2 p-8  bg-amber-200 ml-auto mr-auto mt-7 rounded-xl">
+          <span>
+            <input
+              type="text"
+              value={input}
+              onChange={(e) => e.target.value}
+              className=" text-amber-700 p-2 rounded-lg  border-2 w-auto outline-none"
+            />
+            <button className=" bg-blue-800 text-white font-bold p-2 hover:bg-blue-700 rounded-lg">
+              copy
+            </button>
+          </span>
+          <div className="mt-2 ">
+            <input
+              type="range"
+              value={charlength}
+              min={5}
+              max={50}
+              onChange={(e) => {
+                setCharLength(e.target.value);
+              }}
+            />
+            <label htmlFor="">Length({charlength})</label>
+
+            <input type="checkbox" className="mr-1 ml-2" />
+            <label htmlFor="">Number</label>
+
+            <input type="checkbox" className="mr-1 ml-2" />
+            <label htmlFor="">Character</label>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
