b0bdae3be62c80e2b9420a667eb9450b31d30d09
ShivanshuRag <ragsingh773@gmail.com>
2025-06-24 23:07:42 +0530

    py

--- python/greet.py
+++ python/greet.py
@@ -1,7 +1,44 @@
-print("hello Shivanshu ! ") 
-a = 3 + 4j  # in math i say ivota , Integer , float  complexinterger 
-print(type(a))
+#print("hello Shivanshu ! ") 
+# a = 3 + 4j  # in math i say ivota , Integer , float  complexinterger 
+# print(type(a))
 
 """
-fkdwkddddd
+-> in python everything is object 
+-> Integer ( int , float, complex )
+-> String ( str )
+-> boolean ( bool)
+
 """
+# conversion 
+
+# x = ord('A')
+# print(x)
+
+# y = oct(25)
+# print(y)
+
+# x = chr(65)
+# print(x)
+
+# x = bin(25)
+# print(x)
+
+
+# operator 
+# x = 3 and 4
+# print(x)
+
+# x = 3 or 4
+# print(x)
+
+x = int(input("Enter the marks :  "))
+
+if x > 90:
+    print('A')
+elif x > 80:
+    print('B')
+elif x > 70:
+    print('C')  
+else:
+    print('D')          
+
